ee217903c7808ffaf56013920c75efc679fd1665
keerthana <keerthanayadlapalli896@gmail.com>
2026-01-26 16:25:09 +0530

    Exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -3,10 +3,9 @@ const app =express();
 app.use("/home",(req,res)=>{
      res.send('this is home page');
 });
-app.use("/test",(req,res)=>{
-     res.send('this is test page');
+app.delete("/user",(req,res)=>{
+     res.send("Data got deleted from the database");
 });
-
 app.listen(7777,()=>{
     console.log('Server is running on port 7777');
 });
