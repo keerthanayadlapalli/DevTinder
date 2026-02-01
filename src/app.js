2f5ff1c85556465b1b88672a80879fe3c24865c4
keerthana <keerthanayadlapalli896@gmail.com>
2026-02-01 12:17:29 +0530

    Refactor user route to include middleware for logging and response handling

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app =express();
-app.use("/user/:userId/:name/:password",(req,res)=>{
-     console.log(req.params);
-     res.send('this is keerthana yadlapalli');
-});
+app.use("/user",(req,res,next)=>{
+    console.log("1st print statement");
+      next();
+    res.send("1st response");
+      
+    },(req,res)=>{
+    console.log("2nd print statement");
+    res.send("2nd response");
+    });
+
 app.listen(7777,()=>{
     console.log('Server is running on port 7777');
-});
+}); 
