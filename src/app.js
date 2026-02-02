0aafeb45a14b54b24ea38b2b1c0824c9386761b1
keerthana <keerthanayadlapalli896@gmail.com>
2026-02-02 10:06:12 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app =express();
-app.use("/user",(req,res,next)=>{
-    console.log("1st print statement");
-      next();
-    res.send("1st response");
-      
-    },(req,res)=>{
-    console.log("2nd print statement");
-    res.send("2nd response");
-    });
+const {adminAuth,userAuth}=require('../middlewares/auth.js');
+app.use("/admin",adminAuth);
+app.delete("/admin/deleteAdmin",(req,res)=>{
+    res.send("Admin Deleted Successfully");
+});
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("Hello from DevTinder Admin Panel");;
+});
+app.use("/user/data",userAuth,(req,res)=>{
+    res.send("user data sent successfully");
+});
+app.use("/user/login",(req,res)=>{
+    res.send("user logged in Successfully");
+});
 
 app.listen(7777,()=>{
     console.log('Server is running on port 7777');
