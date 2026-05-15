c88b283f00868f9148ce69bb322fd03eaa3317e8
keerthana <keerthanayadlapalli896@gmail.com>
2026-05-15 12:59:34 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -2,18 +2,19 @@ const express = require('express');
 const connectDB = require('./config/database');
 const app =express();
 const User = require('./models/user');
+app.use(express.json());
 app.post('/signup',async(req,res)=>{
-    const user = new User({
-        firstName:"Keerthana",
-        lastName:"Yadlapalli",
-        emailId:"keerthanayadlapalli896@gmail.com",
-        password:"keerthana123",
-        age:21,
-        gender:"Female"
-    })
+ const user = new User(req.body);
+ try{
     await user.save();
     res.send("User created successfully");
-})
+ }
+ catch(err){
+    res.status(400).send("Error creating user");
+ }
+
+
+});
 connectDB().then(()=>{
     console.log("DB connected successfully");
     app.listen(7777,()=>{
