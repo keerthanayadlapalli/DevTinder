1daf4e8fc5a9067035c26c2c02a65da32176270a
keerthana <keerthanayadlapalli896@gmail.com>
2026-06-24 18:40:30 +0530

    res.json instead of res.send

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -21,7 +21,10 @@ profileRouter.patch('/profile/edit',userAuth,async(req,res)=>{
         const loggedInUser = req.user;
         Object.keys(req.body).forEach(key=>(loggedInUser[key]= req.body[key]));
         await loggedInUser.save();
-        res.send(`${loggedInUser.firstName} your profile updated successfully`);
+        res.json({
+    message: `${loggedInUser.firstName} your profile updated successfully`,
+    data: loggedInUser
+});
 
     }
     catch(err){
