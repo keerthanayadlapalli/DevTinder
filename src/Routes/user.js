623d46bda70aad5802d180ab8869f27dfbadee86
keerthana <keerthanayadlapalli896@gmail.com>
2026-07-01 00:24:52 +0530

    Pagination in feed

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -52,6 +52,10 @@ userRouter.get('/user/connections',userAuth,async (req,res)=>{
 userRouter.get('/feed',userAuth,async(req,res)=>{
     try{
         const loggedInUser = req.user;
+        const page = parseInt(req.query.page);
+        let limit = parseInt(req.query.limit);
+        limit = limit>50 ? 50 : limit;
+        const skip = (page-1)*limit;
         //Finding all the connection requests(send+received)
         const connectionRequests = await ConnectionRequest.find({
             $or :[
@@ -70,7 +74,7 @@ userRouter.get('/feed',userAuth,async(req,res)=>{
                 {_id:{$nin: Array.from(hideUsersFromFeed)}},
                 {_id:{$ne : loggedInUser._id}}
             ]
-        }).select("firstName lastName");
+        }).select("firstName lastName").skip(skip).limit(limit);
         res.send(users);
 
     }
